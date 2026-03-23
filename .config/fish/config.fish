e84e9331299104b1dc36734e8b1ace533c2201ed
Adnan <contact@aadnanmt.web.id>
2026-03-24 00:05:37 +0700

    chore: simple alias for commandline git dif

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -80,6 +80,8 @@ alias gal='git add .'
 alias gc='git commit -m'
 alias gp='git push'
 alias gl='git pull'
+alias gf='git diff'
+alias gf='git diff .'
 alias gac='git add . && git commit -m'
 alias glog="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 
