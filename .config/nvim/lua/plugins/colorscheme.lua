ff0afb96cfc72492fd43cccef79e611fb585e4b3
Adnan Slamet Wibowo <contact@aadnanmt.web.id>
2026-04-23 10:51:56 +0700

    feat(nvim): implement cyberdream colorscheme for cyan aesthetic

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("cyberdream").setup({
+        transparent = true,
+        italic_comments = true,
+        hide_fillchars = true,
+        borderless_telescope = true,
+        terminal_colors = true,
+      })
+      vim.cmd("colorscheme cyberdream")
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "cyberdream",
+    },
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      opts.options.theme = "cyberdream"
+    end,
+  },
+}
